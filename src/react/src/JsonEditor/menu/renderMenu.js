fcbbf00c238b30e824936d361498447fb5338119
oscar <oscarzhou0727@gmail.com>
2023-09-14 15:36:42 +1200

    feat(ui): add refresh button

--- src/react/src/JsonEditor/menu/renderMenu.js
+++ src/react/src/JsonEditor/menu/renderMenu.js
@@ -1,39 +1,67 @@
-import {faClose, faDownload, faPlus, faUpload} from "assets/icons";
+import { faClose, faDownload, faPlus, faUpload, faRotateRight } from "assets/icons";
 
-export const renderMenuFactory = ({getData, onShowFileUploader, Id, newEditor, closeEditor, editor}) => (items, context) => {
-    const separator = {type: 'separator'}
+export const renderMenuFactory =
+  ({
+    getData,
+    postData,
+    onDownloadFile,
+    onShowFileUploader,
+    Id,
+    newEditor,
+    closeEditor,
+    editor,
+  }) =>
+  (items, context) => {
+    const separator = { type: "separator" };
+
+    const refreshButton = {
+      type: "button",
+      icon: faRotateRight,
+      title: "Refresh",
+      onClick: getData,
+    };
 
     const downloadButton = {
-        type: "button",
-        icon: faDownload,
-        title: "Download",
-        onClick: getData,
-    }
+      type: "button",
+      icon: faDownload,
+      title: "Download",
+      onClick: () => onDownloadFile(),
+    };
 
     const uploadButton = {
-        type: "button",
-        icon: faUpload,
-        title: "Upload",
-        onClick: () => onShowFileUploader(),
-    }
+      type: "button",
+      icon: faUpload,
+      title: "Upload",
+      onClick: () => onShowFileUploader(),
+    };
 
     const newEditorButton = {
-        type: "button",
-        icon: faPlus,
-        title: "New Editor",
-        onClick: () => {
-            newEditor(Id)
-        },
-    }
+      type: "button",
+      icon: faPlus,
+      title: "New Editor",
+      onClick: () => {
+        newEditor(Id);
+      },
+    };
 
     const closeEditorButton = {
-        type: "button",
-        icon: faClose,
-        title: "Close Editor",
-        onClick: () => {
-            closeEditor(Id)
-        },
-    }
+      type: "button",
+      icon: faClose,
+      title: "Close Editor",
+      onClick: () => {
+        closeEditor(Id);
+      },
+    };
 
-    return [newEditorButton, closeEditorButton, separator, downloadButton, uploadButton, separator, ...items]
-}
+    return [
+      newEditorButton,
+      closeEditorButton,
+      separator,
+      refreshButton,
+      separator,
+      downloadButton,
+      uploadButton,
+      separator,
+      ...items,
+    ];
+  };
